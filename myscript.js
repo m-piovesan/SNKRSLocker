15818a03a5af863b7552fc4fc4d4d3a7abf1ad94
Matheus Piovesan <97471131+m-piovesan@users.noreply.github.com>
2023-03-13 14:51:26 -0300

    Add files via upload

---
+++ myscript.js
@@ -0,0 +1,41 @@
+var clock;
+		
+$(document).ready(function() {
+    var clock;
+
+    clock = $('.countdown').FlipClock({
+        clockFace: 'DailyCounter',
+        autoStart: false,
+        callbacks: {
+            stop: function() {
+                $('.message').html('A BLACK FRIDAY JÁ COMEÇOU')
+            }
+        }
+    });
+            
+    clock.setTime(604800);
+    clock.setCountdown(true);
+    clock.start();
+
+});
+
+$('.message a').click(function(){
+    $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
+});
+
+
+// FOI AQUI PRA BAIXO QUE EU TENTEI |
+//                                  V
+
+function screenSize(){
+    var windowWidth = window.innerWidth;
+
+    document.querySelector('.window-size').innerHTML = windowWidth + 'x' + windowHeight;
+
+};
+
+screenSize();
+
+window.addEventListener('resize', function(){
+	screenSize();
+});
